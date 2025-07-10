5b968de27dd1d962c1ed5ab92d1bfb1e8ffbf016
RobiulHassan0 <robiulhassan270@gmail.com>
2025-07-10 21:58:53 +0600

    explore more function php

--- module_02/function.php
+++ module_02/function.php
@@ -1,7 +1,7 @@
 <?php
 
-// Function in PHP
-// Function Structure: function functionName($parameters) { // code }
+// Function in PHP | 1. built-in Functions 2. User-defined Functions
+// User-defined Function Structure: function functionName($parameters) { // code }
 function greeting ($name){
     echo "Hello $name, welcome to PHP programming! \n";
 }
@@ -20,3 +20,23 @@ function sum ($a, $b){
 sum(a:10, b:20);
 sum(a:15, b:50);
 sum(a:25, b:75);
+
+
+
+// built-in Functions
+// count() is a built-in function to count elements in an array
+$colors = ["red", "green", "blue", "yellow", "purple"];
+echo "The length of the array (total color) is: ", count($colors), "\n"; 
+
+for($i =0; $i<count($colors); $i++){
+    echo "Color at index $i is: ", $colors[$i], "\n";
+}
+
+// built-in function to get Date and Time
+echo time(), "\n"; // returns the current timestamp
+echo date(format: "D-m-y H:i:s"), "\n"; // returns the current date and time in the specified format
+
+// to uppercase a string
+echo strtoupper(string: "hello world"), "\n"; // converts the string to uppercase
+// to lowercase a string
+echo strtolower(string: "HELLO WORLD"), "\n"; // converts the string to lowercase
