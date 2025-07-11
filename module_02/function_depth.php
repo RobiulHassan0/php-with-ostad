725178da202ee01c898672c61eda9aa80b6ad978
RobiulHassan0 <robiulhassan270@gmail.com>
2025-07-11 16:27:29 +0600

    explore global and local scope

--- module_02/function_depth.php
+++ module_02/function_depth.php
@@ -14,3 +14,25 @@ function sum(int $a, float $b): int|float{
     return $a + $b;
 }
 echo sum(a: 10, b: 20.5), "\n"; // Outputs: 30.5
+
+
+// introducing Global Scope and Local Scope
+$gScope = "this is a global variable";
+function lscopeExample(): void
+{
+    $localeScope = "this is a local variable";
+    global $gScope; // Accessing global variable inside the function
+    echo "Inside the function: $localeScope \n";
+    echo "Accessing global variable: $gScope \n";
+}
+lscopeExample();
+
+$globalName = "Robiul Hassan";
+function greetUser(): string
+{
+    global $globalName; // Accessing global variable inside the function
+    // Using the global variable to create a greeting message
+    return "Hello, $globalName! Welcome to PHP programming. \n";
+}
+// Function call
+echo greetUser();
