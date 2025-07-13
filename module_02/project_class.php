67c7c8003cba5d5895166ac75b0209b0a6648d6b
RobiulHassan0 <robiulhassan270@gmail.com>
2025-07-13 23:56:35 +0600

    project explore

--- module_02/project_class.php
+++ module_02/project_class.php
@@ -6,4 +6,6 @@ $contacts = [];
 function addContact(array &$contacts, string $name, string $email, $phone){
     $contacts = ['name' => $name, 'email' => $email, 'phone' => $phone];
     array_push($contacts, ['name' => $name, 'email' => $email, 'phone' => $phone]);
+    var_dump($contacts);
+    return $contacts;
 }
