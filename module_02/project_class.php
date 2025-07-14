7ff1bd56552301db9815f9779e6464760ebb2825
RobiulHassan0 <robiulhassan270@gmail.com>
2025-07-15 00:46:11 +0600

    done contact app

--- module_02/project_class.php
+++ module_02/project_class.php
@@ -3,9 +3,60 @@
 // Contact App
 $contacts = [];
 
-function addContact(array &$contacts, string $name, string $email, $phone){
-    $contacts = ['name' => $name, 'email' => $email, 'phone' => $phone];
+function addContact(array &$contacts, string $name, string $email, $phone):void
+{
+    // $contacts = ['name' => $name, 'email' => $email, 'phone' => $phone];
     array_push($contacts, ['name' => $name, 'email' => $email, 'phone' => $phone]);
-    var_dump($contacts);
-    return $contacts;
+}
+
+function displayContacts(array $contacts):void{
+    if(empty($contacts)){
+        echo "No Contact Available \n";
+    }
+    else{
+        $contactCount = 1;
+        foreach($contacts as $contact){
+            // echo "
+            // Name: {$contact['name']},
+            // Email: {$contact['email']},
+            // Phone: {$contact['phone']} \n";
+            echo "Contact {$contactCount}\n";
+            echo "--------------------------\n";
+            echo "Name: {$contact['name']}\n";
+            echo "Email: {$contact['email']}\n";
+            echo "Phone: {$contact['phone']}\n";
+            echo "--------------------------\n";
+
+            $contactCount++;
+        }
+    }
+}
+
+while(true){
+    echo "\nContact Management App \n";
+    echo "1. Add Contact\n";
+    echo "2. View Contact\n";
+    echo "3. Exit\n";
+
+    $choice = (int)readline("Choice An Option: ");
+    
+    if($choice === 1){
+        $name = readline("Enter Your Name : ");
+        $email = readline('Enter Your Email : ');
+        $phone = readline("Enter Your Phone Number : ");
+        addContact($contacts, $name, $email, $phone);
+
+        // echo "$phone added to Contacts. \n";
+        // echo "$name added to Contacts. \n";
+        // echo "$email added to Contacts. \n";
+        echo "Contact created successfully \n";
+    }elseif($choice === 2){
+        displayContacts($contacts);
+    }elseif($choice === 3){
+        echo "Exited\n";
+        break;
+    }
+    else{
+        echo "Invalid Choice. please try again \n";
+    }
 }
