e5ae90928330408a5fe70dbb6e32b5ecebcea847
RobiulHassan0 <robiulhassan270@gmail.com>
2025-07-25 23:57:36 +0600

    added

--- module_03/C2_crud.php
+++ module_03/C2_crud.php
@@ -9,18 +9,53 @@ fwrite($fileRecourse, "Hello, this is test file. \n");  // Write first line to t
 fwrite($fileRecourse, "Second Line Added. \n");  // Write second line to the file
 fclose($fileRecourse);  // Always close the file after writing to save changes and free system resources
 
+
 // Step 2: File Reading (Either fread OR line-by-line)
 
-// Method 1: Read Entire File
+// Method 1: Only first line Reading
+$fileRecourse = fopen($fileName, 'r');
+echo "--- only first line---\n";
+echo fgets($fileRecourse);
+fclose($fileRecourse);
+echo "\n";
+
+// Method 2: Read Entire File
 $fileRecourse = fopen($fileName, 'r');
 $content = fread($fileRecourse, filesize("example.txt"));
+echo "--- Full File Read --- \n";
 echo $content;
 fclose($fileRecourse);
 
-// Method 2: Line by Line Reading
+
+// Method 3: Line by Line Reading
 $fileRecourse = fopen($fileName, 'r');
 echo "\n--- Line by Line Reading ---\n";
 while(!feof($fileRecourse)){
     echo fgets($fileRecourse);
 }
 fclose($fileRecourse);
+
+
+// step 3: Content Add
+$fileOpen = fopen($fileName, 'a');
+$fileHandle = fwrite($fileOpen, "Added Content");
+fclose($fileOpen);
+
+
+// step 4: File Delete
+if(file_exists($fileName)){
+    unlink($fileName);
+}else{
+    echo "File does not exist.";
+}
+
+
+// combination of 3 function- fopen(), fwrite(), fclose()
+
+$fileName = "hello.txt";
+file_put_contents($fileName, 'Hello PHP');
+
+// file Append
+file_put_contents($fileName, "\nHello PHP", FILE_APPEND);
+// file read
+echo file_get_contents($fileName);
---
+++ module_03/C2_session.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Step 1: Start session
+session_start(); 
+
+Step 2: Save data in session
+$_SESSION['username'] = 'robin';
+
+Step 3: Access anywhere
+echo $_SESSION['username']; // Output: robin
+*/
+
+session_start();
+$_SESSION['user'] = ['name' => 'robin', 'age' => 42, 'role' => 'dev'];
+
+unset($_SESSION['user']['age']);
+
+print_r($_SESSION['user']);
