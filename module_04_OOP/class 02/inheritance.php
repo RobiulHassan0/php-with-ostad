f0b4356e18498faf25af0b9b2c9c538f97ab988a
RobiulHassan0 <robiulhassan270@gmail.com>
2025-08-07 08:48:48 +0600

    class unserstadning done

--- module_04_OOP/class 02/inheritance.php
+++ module_04_OOP/class 02/inheritance.php
@@ -8,15 +8,38 @@ class Student{
     public $class;
 
     public function studentDetails(){
-        echo "student name: ". $this->name;
-        echo "student department: ". $this->department;
-        echo "student class: ". $this->class;
+        echo "Educational Background: ". $this->educBackground . "\n";
+        echo "student department: ". $this->department . "\n";
+        echo "student class: ". $this->class . "\n";
     }
 
 }
 
 // Step 2: Create a Child Class
-class StudentInfo{
+class StudentInfo extends Student{
     public $name;
-    public $
+    public $roll;
+    public $id;
+
+    public function studentInformation()
+    {
+        echo "Student name: ". $this->name . "\n";
+        echo "Student roll: ". $this->roll . "\n";
+        echo "Student id: ". $this->id . "\n";
+    }
+
 }
+
+// Step 3: Create Object and set data with properties of child class 
+$student1 = new StudentInfo();
+$student1->name = "Robiul Hassan";
+$student1->roll = 835;
+$student1->id = 101;
+$student1->educBackground = "Qawmi Madrasa";
+$student1->department = "Dars-E-Nizami";
+$student1->class = "Takmil";
+
+// Step 4: Call Methods of Parent Class
+$student1->studentDetails(); // parent class's method
+echo "\n";
+$student1->studentInformation(); // child class's method
