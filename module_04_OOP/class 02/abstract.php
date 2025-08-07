cef8fed9396864d44eae2f34ce3d1e4cd792387f
RobiulHassan0 <robiulhassan270@gmail.com>
2025-08-08 00:34:39 +0600

    access modifires  unserstadning done

--- module_04_OOP/class 02/abstract.php
+++ module_04_OOP/class 02/abstract.php
@@ -1,2 +1,46 @@
 <?php
 // introduction to abstract class in PHP OOP
+// step 1: create a parent class
+abstract class Shape{
+    // create area for every shape
+    abstract public function area();
+}
+
+// Step 2: Create a Concrete Class- Circle
+class Circle extends Shape{
+    public $radius;
+
+    public function __construct($radius)
+    {
+        $this->radius = $radius;
+    }
+
+    public function area()
+    {
+        return pi() * pow($this->radius, 2);
+    }
+}
+
+// step 3: Concrete Class- Rectangle
+class Rectangle extends Shape{
+    public $width;
+    public $height;
+
+    public function __construct($width, $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+    
+    public function area()
+    {
+        return $this->width * $this->height;
+    }
+} 
+
+// Step 4: Create Object and set data with properties of child class
+$circle = new Circle(5);
+echo "Circle Area: ". $circle->area() . "\n";
+
+$rectAngle = new Rectangle(4, 6);
+echo "Rectangle Area: ". $rectAngle->area() . "\n";
