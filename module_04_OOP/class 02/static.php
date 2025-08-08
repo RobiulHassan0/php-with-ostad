dc8243d22eb7e82504d6ecc9917f3ce3f9b93a3b
RobiulHassan0 <robiulhassan270@gmail.com>
2025-08-08 17:29:01 +0600

    static  understanding done

--- module_04_OOP/class 02/static.php
+++ module_04_OOP/class 02/static.php
@@ -1,2 +1,16 @@
 <?php
 // introduction to static property and method in PHP OOP
+// step 1: Static Property. it is a property that is shared by all instances of a class.
+// step 2: Static Method. it is a method that is shared by all instances of a class.
+
+// step 2: Static Property)
+class Name{
+    public static $name = "Abu Zayed";  // this is called as a static property
+
+    public static function getName(){ // this is called as a static method
+        return self::$name;
+    }
+}
+echo Name::$name . "\n"; // directly access static property, not need to create an object
+echo Name::getName() . "\n"; // same rule applicable in static method.
+
