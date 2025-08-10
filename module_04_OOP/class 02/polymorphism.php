4573d5569434e27084b5953b7555ad5af9e77331
RobiulHassan0 <robiulhassan270@gmail.com>
2025-08-10 23:53:32 +0600

    understanding  polymorphism

--- module_04_OOP/class 02/polymorphism.php
+++ module_04_OOP/class 02/polymorphism.php
@@ -1,3 +1,20 @@
 <?php
 
 // Introduce to Polymorphism in PHP OOP
+class Person{
+    public function introduce(){
+        echo "I am a person \n";
+    }
+}
+
+class Student extends Person{
+    public function introduce(){
+        echo "I am a student \n";
+    }
+}
+
+class Employee extends Person{
+    public function introduce(){
+        echo "I am an employee \n";
+    }
+}
