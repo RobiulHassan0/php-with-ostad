5f75622882857e5d71fcda4e09d4436b0398f065
Robiul Hassan <robiulhassan270@gmail.com>
2025-08-26 04:37:22 +0600

    done answere 'D' of first  Question

--- module_06_Database/My_Queries.sql
+++ module_06_Database/My_Queries.sql
@@ -26,3 +26,23 @@ WHERE STATUS = 'ACTIVE'
 GROUP BY OPEN_BRANCH_ID
 HAVING AVG(PENDING_BALANCE) < 4300;
 
+
+
+-- Q1(D): Pagination query | OFFSET starts from 0
+-- Example of showing 6 employees per page | Total 18 rows here
+
+-- Page 1: Start from row 0 | shows 6 rows (Employee ID 1 to 6)
+SELECT * FROM employee
+LIMIT 6
+OFFSET 0;
+
+-- Page 2: Start from row 6 | shows next 6 rows (Employee ID 7 to 12)
+SELECT * FROM employee
+LIMIT 6
+OFFSET 6;
+
+-- Page 3: Start from row 12 | shows next 6 rows (Employee ID 13 to 18)
+SELECT * FROM employee
+LIMIT 6
+OFFSET 12;
+
