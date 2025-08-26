172e466e582d5a3bccc4f50485fcea6e3ea41a03
Robiul Hassan <robiulhassan270@gmail.com>
2025-08-27 01:32:31 +0600

    start integrate mySQL-PHP

---
+++ module_07/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$localhost = 'localhost';
+$username = 'robi';
+$password = 'robi';
+$database = 'corephp';
+
+try{
+    $connection = new mysqli($localhost, $username, $password, $database);
+}catch(Exception $e){
+    echo $e->getMessage();
+}
+
+// $connection = new mysqli($localhost, $username, $password, $database);
+// if($connection->connect_error){
+//     echo $connection->connect_error;
+// }
+// echo 'Connected';
---
+++ module_07/index.php
@@ -0,0 +1,11 @@
+<?php
+echo 'PHP MYSQL';
+
+/*
+* mysql database connection with php - done
+* data insert with validation
+* get single data
+* get all data
+* data update
+* data delete
+*/
