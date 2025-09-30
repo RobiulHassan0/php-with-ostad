cb40df840be0a05d6390a1bd6d0983431a221cf3
RobiulHassan0 <robiulhassan270@gmail.com>
2025-09-30 16:49:17 +0600

    inserted data into tables users and tasks

--- module_14/task manager/TaskManagerDB.session.sql
+++ module_14/task manager/TaskManagerDB.session.sql
@@ -1,6 +1,16 @@
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(150) NOT NULL UNIQUE,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+INSERT INTO users (name, email) VALUES
+('Asadul Islam', 'asad@mail.com'),
+('Fatema Noor', 'fatema@mail.com'),
+('Kamal Hossain', 'kamal@mail.com');
+
+INSERT INTO tasks (user_id, title, description, status) VALUES
+(1, 'Database Bakcup', 'taking backup fo servers', 0),
+(1, 'Create Presentation', 'making slides', 1),
+(1, 'Project Research', 'Resouces Collection', 0),
+
+(2, 'Web Design', 'Checking Hompage', 0),
+(2, 'Code Review', 'Projcet Code Review', 1), 
+
+(3, 'Users Feedbacks', 'sending form link', 1),
+(3, 'Bug Fixing', 'maintaining Login bugs', 0),
+(3, 'Doqumantion', 'designing README File', 0);
